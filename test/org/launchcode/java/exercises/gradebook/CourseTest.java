1529ccf4e65e106af17d37239de4aa3ea7921f46
Chris Bay <chris@chrisbay.net>
2017-01-28 12:25:58 -0600

    Add Student and Course tests

---
+++ test/org/launchcode/java/exercises/gradebook/CourseTest.java
@@ -0,0 +1,88 @@
+package org.launchcode.java.exercises.gradebook;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by LaunchCode
+ */
+public class CourseTest {
+
+    @Test
+    public void testCourseInit() {
+        Course c = new Course("CSE131", 1, 2);
+        assertEquals("CSE131", c.getName());
+        assertEquals(2, c.getRemainingSeats());
+        for (int i = 0; i < 20; ++i) {
+            double a =  Math.random() * 5000;
+            int s = (int) (Math.random() * 5000);
+            Course c2 = new Course("" + a, s, s);
+            assertEquals(String.valueOf(a), c2.getName());
+            assertEquals(s, c2.getRemainingSeats());
+        }
+    }
+
+    @Test
+    public void testAddStudent() {
+        for (int i = 0; i < 100; i++) {
+            double a =  (Math.random() * 5000);
+            int s = (int)(Math.random() * 50);
+            Course c = new Course("" + a, s, s);
+
+            for (int j = 0; j < s; j++) {
+                String aa =  "" + (Math.random() * 5000);
+                String b =  "" + (Math.random() * 5000);
+                int cc = (int) (Math.random() * 500000);
+                Student s2 = new Student(aa, b, cc);
+                boolean added = c.addStudent(s2);
+                assertTrue(added);
+
+                assertEquals(s - j - 1, c.getRemainingSeats());
+            }
+
+            //Try to add students, even though the class is full.
+            for (int j = 0; j < s; j++) {
+                String aa =  "" + (Math.random() * 5000);
+                String b =  "" + (Math.random() * 5000);
+                int cc = (int) (Math.random() * 500000);
+                Student s2 = new Student(aa, b, cc);
+                boolean added = c.addStudent(s2);
+
+                assertTrue(!added);
+            }
+        }
+    }
+
+    @Test
+    public void testAverageGPA() {
+        for (int j = 0; j < 100; j++) {
+            double a =  (Math.random() * 5000);
+            int s = (int)(Math.random() * 50 + 50);
+            Course c = new Course("" + a, s, s);
+
+            double gpaSum = 0;
+            for(int i = 0; i < s; i++) {
+                String aa =  "" + (Math.random() * 5000);
+                String b =  "" + (Math.random() * 5000);
+                int cc = (int)(Math.random() * 500000);
+                Student s2 = new Student(aa, b, cc);
+                double g = Math.round(Math.random() * 4000) / 1000.0;
+                gpaSum += g;
+                s2.submitGrade(g, 1);
+                c.addStudent(s2);
+                assertEquals(gpaSum / (i + 1), c.averageGPA(), 0.01);
+            }
+        }
+    }
+
+    @Test
+    public void testCourseToString() {
+        for (int i = 0; i < 100; i++) {
+            double a =  (Math.random() * 5000);
+            int c = (int) (Math.random() * 500000);
+            Course cc = new Course("" + a, c, c);
+            assertTrue(cc.toString().contains("" + a));
+            assertTrue(cc.toString().contains("" + c));
+        }
+    }
+}
---
+++ test/org/launchcode/java/exercises/gradebook/StudentTest.java
@@ -0,0 +1,139 @@
+package org.launchcode.java.exercises.gradebook;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by LaunchCode
+ */
+public class StudentTest {
+
+    private static double delta = 0.01;
+
+    @Test
+    public void testStudentInit() {
+        Student s = new Student("Doug", "Shook", 111111);
+        assertEquals("Doug Shook", s.getName());
+        assertEquals(111111, s.getStudentID());
+        //No credits, should be a freshman, no GPA
+        assertEquals(0.0, s.getGPA(), delta);
+        assertEquals(0, s.getCredits());
+        assertEquals("Freshman", s.getClassStanding());
+        for (int i = 0; i < 20; ++i) {
+            double a =  (Math.random() * 5000);
+            double b =  (Math.random() * 5000);
+            int c = (int)(Math.random() * 5000);
+            Student s3 = new Student("" + a, "" + b, c);
+            assertEquals(a + " " + b, s3.getName());
+            assertEquals(0.0, s3.getGPA(), delta);
+            assertEquals(0, s3.getCredits());
+            assertEquals("Freshman", s3.getClassStanding());
+        }
+    }
+
+    @Test
+    public void testGetClassStanding() {
+        Student s = new Student("D", "S", 1);
+        for (int i = 0; i < 29; i++) {
+            s.submitGrade(1.0, 1);
+            assertEquals("Freshman", s.getClassStanding());
+        }
+
+        for (int i = 0; i < 30; i++) {
+            s.submitGrade(1.0, 1);
+            assertEquals("Sophomore", s.getClassStanding());
+        }
+
+        for (int i = 0; i < 30; i++) {
+            s.submitGrade(1.0, 1);
+            assertEquals("Junior", s.getClassStanding());
+        }
+
+        for (int i = 0; i < 30; i++) {
+            s.submitGrade(1.0, 1);
+            assertEquals("Senior", s.getClassStanding());
+        }
+
+    }
+
+    @Test
+    public void testSubmitGrade() {
+        Student s = new Student("D", "S", 1);
+        int credits = 0;
+        double gpatotal = 0;
+        for (int i = 0; i < 100; i++) {
+            int c = (int)(Math.random() * 3 + 1);//1 to 3 credits
+            double g = Math.random() * 4;//0 to 4
+            credits += c;
+            gpatotal += g * c;
+            s.submitGrade(g, c);
+            assertEquals("GPA computed incorrectly", gpatotal / credits, s.getGPA(), 0.01);
+            assertTrue("GPA not rounded", (s.getGPA() + "").length() < 6);
+        }
+    }
+
+    @Test
+    public void testComputeTuition() {
+        Student s = new Student("D", "S", 1);
+        for (int i = 0; i < 14; i++) {
+            s.submitGrade(0, 1);
+            assertEquals((i+1) * 1333.33, s.computeTuition(), delta);
+        }
+
+        s.submitGrade(0, 1);
+        assertEquals(20000.0, s.computeTuition(), delta);
+
+        for (int i = 0; i < 14; i++) {
+            s.submitGrade(0, 1);
+            assertEquals(1333.33 * (i+1) + 20000.0, s.computeTuition(), delta);
+        }
+    }
+
+    @Test
+    public void testCreateLegacy() {
+        for(int i = 0; i < 100; i++) {
+            double a =  (Math.random() * 5000);
+            double b =  (Math.random() * 5000);
+            Student s = new Student("" + a, "" + b, 1);
+            double a2 =  (Math.random() * 5000);
+            double b2 =  (Math.random() * 5000);
+            Student ss = new Student("" + a2, "" + b2, 2);
+            int c = (int)(Math.random() * 120 + 1);
+            double g = Math.round(Math.random() * 4000) / 1000.0;
+            s.submitGrade(g, c);
+            int c2 = (int)(Math.random() * 120 + 1);
+            double g2 = Math.round(Math.random() * 4000) / 1000.0;
+            ss.submitGrade(g2, c2);
+            Student bb = s.createLegacy(s, ss);
+            assertTrue(bb.getName().contains(s.getName()) && bb.getName().contains(ss.getName()));
+            assertEquals((g + g2) / 2, bb.getGPA(), 0.01);
+            assertEquals(bb.getCredits(), Math.max(c, c2));
+            assertTrue(bb.getStudentID() == s.getStudentID() + ss.getStudentID());
+
+            //Make sure parents haven't changed
+            assertEquals(a + " " + b, s.getName());
+            assertEquals(g, s.getGPA(), delta);
+            assertEquals(c, s.getCredits());
+
+            assertEquals(a2 + " " + b2, ss.getName());
+            assertEquals(g2, ss.getGPA(), delta);
+            assertEquals(c2, ss.getCredits());
+
+        }
+    }
+
+    @Test
+    public void testStudentToString() {
+        for (int i = 0; i < 100; i++) {
+            double a =  (Math.random() * 5000);
+            double b =  (Math.random() * 5000);
+            int c = (int) (Math.random() * 500000);
+            Student s = new Student("" + a, "" + b, c);
+            assertTrue(s.toString().contains(String.valueOf(a)));
+            assertTrue(s.toString().contains(String.valueOf(b)));
+            assertTrue(s.toString().contains(String.valueOf(c)));
+        }
+    }
+
+}
